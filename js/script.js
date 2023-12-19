2b1381d1fa10d8b0a2c4f58d29331e56bba0b94d
Mitch <mitch@hawkins5.com>
2023-12-19 15:34:19 +1100

    Got On Blur working for start menu

--- js/script.js
+++ js/script.js
@@ -5,8 +5,19 @@ startButton.addEventListener("click", () => {
   toggleStart(startMenu);
 });
 
+startButton.addEventListener("blur", () => {
+  const startMenu = document.querySelector("#startMenu");
+  removeStart(startMenu);
+});
+
 function toggleStart(startMenu) {
   if (startMenu == null) return;
   startMenu.classList.toggle("start__active");
   startButton.classList.toggle("nav__start__active");
+}
+
+function removeStart(startMenu) {
+  if (startMenu == null) return;
+  startMenu.classList.remove("start__active");
+  startButton.classList.remove("nav__start__active");
 }
