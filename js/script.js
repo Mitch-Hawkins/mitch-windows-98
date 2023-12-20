5d16f43df0f2f780b72923c9c354c9ce1f92dc06
Mitch <mitch@hawkins5.com>
2023-12-20 15:59:05 +1100

    Added first Recycle Bin modal, finally got program bar to appear and dissappear correctly

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 const startButton = document.querySelector("#startButton");
+const recycleBinShortcut = document.querySelector("#recycleBin");
+
+// const openProgram = document.querySelector(".desktop__article");
+const closeButton = document.querySelector(".desktop__modal__header__button");
+
+const navBar = document.querySelector("#program-bar");
 
 startButton.addEventListener("click", () => {
   const startMenu = document.querySelector("#startMenu");
@@ -20,4 +26,49 @@ function removeStart(startMenu) {
   if (startMenu == null) return;
   startMenu.classList.remove("start__active");
   startButton.classList.remove("nav__start__active");
+}
+
+recycleBinShortcut.addEventListener("click", () => {
+  const recycleBinWindow = document.querySelector("#modal-recyclebin");
+  openRecycleBin(recycleBinWindow);
+});
+
+function openRecycleBin(recycleBinWindow) {
+  if (recycleBinWindow == null) return;
+  if (recycleBinWindow.classList.contains("modal__open")) return;
+
+  recycleBinWindow.classList.add("modal__open");
+  // navBar.innterHTML = `<button class="nav__program__button"></button>`;
+  addProgramToBar("Recycle Bin", "nav__program__button");
+}
+
+function addProgramToBar(programTitle, cls) {
+  const newElement = document.createElement("button");
+  const parentNode = navBar;
+
+  newElement.classList.add(cls);
+
+  const newText = document.createTextNode(programTitle);
+
+  newElement.appendChild(newText);
+
+  parentNode.appendChild(newElement);
+}
+
+closeButton.addEventListener("click", () => {
+  const modal = closeButton.closest(".desktop__modal");
+  const progNodeList = document.getElementsByClassName("nav__program__button");
+  const progBar = Array.from(progNodeList).reduce((acc, node) => acc + node);
+  console.log(progBar);
+  closeWindow(modal);
+  removeProgramFromBar(progBar);
+});
+
+function removeProgramFromBar(programElement) {
+  programElement.remove();
+}
+
+function closeWindow(modal) {
+  if (modal == null) return;
+  modal.classList.remove("modal__open");
 }
